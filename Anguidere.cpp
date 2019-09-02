cd827c6ecfba5fac9ed25a662b8e33576041adfb
Lame Lemon <garybsantos@gmail.com>
2019-09-03 00:03:58 +0300

    Added live and level seeding

--- Anguidere.cpp
+++ Anguidere.cpp
@@ -14,9 +14,29 @@ public:
 	struct coord {float x; float y;};
 	std::vector<coord> trail;
 	int score = 0;
+	int direction = 3;
+	int lives = 3;
 
 public:
-	void boundaryCollision(int32_t ScreenWidth, int32_t ScreenHeight)
+	bool reset(){
+		if (lives > 1) {
+			lives--;
+			x = y = 0.0f;
+			xSpeed = 1.0f;
+			ySpeed = 0.0f;		
+			return true;
+		} 
+		lives = 3;
+		length = 2;
+		score = 0;
+		x = y = 0.0f;
+		xSpeed = 1.0f;
+		ySpeed = 0.0f;
+		direction = 3;
+		trail.push_back({x, y});
+		return false;
+	}
+	bool boundaryCollision(int32_t ScreenWidth, int32_t ScreenHeight)
 	{
 		if (x < 0){
 			x = ScreenWidth - blockSize;
@@ -32,8 +52,10 @@ public:
 			if (trail[i].x == x && trail[i].y == y){
 				length = 2;
 				score = 0;
+				return False;
 			}
-		} 
+		}
+		return True; 
 	}
 	void shift(){
 		coord new_c = {x, y};
@@ -45,6 +67,14 @@ public:
 	void calculatePosition(){
 		x += blockSize * xSpeed;
 		y += blockSize * ySpeed;
+		if (xSpeed > 0)
+			direction = 3;
+		else if (xSpeed < 0)
+			direction = 2;
+		else if (ySpeed > 0)
+			direction = 1;
+		else
+			direction = 0;
 	}
 };
 
@@ -62,6 +92,15 @@ public:
 	}
 };
 
+class Bullet
+{
+public:
+	float x = -10.0f;
+	float y = -10.0f;
+	int direction = 0; // up 0, down 1, left 2, right 3
+	float coolDown = 0.0f;
+};
+
 class Food
 {
 public:
@@ -72,61 +111,81 @@ public:
 	void randomLoc() {
 		x = rand() % 20 * 10;
 		y = rand() % 20 * 10;
-		std::cout << x << " " << y << std::endl;
+		// std::cout << x << " " << y << std::endl;
 	}
 
 	bool collision(float snakeX, float snakeY) {
-		if (x == snakeX && y == snakeY){
-			randomLoc();
+		if (x == snakeX && y == snakeY)
 			return true;
-		}
 		return false;
 
 	}
 };
 
-class SnakeMaze : public olc::PixelGameEngine
+class Anguidere : public olc::PixelGameEngine
 {
 public:
-	SnakeMaze()
+	Anguidere()
 	{
-		sAppName = "Snake Maze";
+		sAppName = "Anguidere";
 	}
 
 	struct lightCoord { float x; float y; float fBlend; };
 
 	int board[31][31] = { 0 };
-	float gameSpeed = 0.20f;
+	float gameSpeed = 0.15f;
 
 	Snake snakeUser;
 	Wall wall;
 	Food fruit;
+	Bullet bullet;
 
 private:
 	float last_update;
 	bool title;
+	int level = 0;
+	int wall_chance = 8;
 public:
-	bool OnUserCreate() override
-	{
-		srand(time(NULL));
 
-		title = true;
+	void reset(){
+		
 		last_update = 0.0f;
+		if(!snakeUser.reset()){
+			level = 0;
+			newLevel();	
+		}
+	}
+
+	void newLevel(){
+		srand(level + 1);
+		std::cout << "Level " << level + 1 << std::endl;
+		last_update = 0.0f;
+
+		snakeUser.x = snakeUser.y = 0.0f;
+		snakeUser.direction = 3;
+		snakeUser.xSpeed = 1.0f;
+		snakeUser.ySpeed = 0.0f;
 		snakeUser.trail.push_back({snakeUser.x, snakeUser.y});
 
+		fruit.randomLoc();
+		while (board[int(fruit.x/snakeUser.blockSize)][int(fruit.y/snakeUser.blockSize)] != 0)
+			fruit.randomLoc();
+		
 		// Initialise walls
-		wall.initWalls();
 		for(int x = 0; x < ScreenWidth()/snakeUser.blockSize; x+= 2){
 			for(int y = 0; y < ScreenHeight()/snakeUser.blockSize; y+= 2){
-				int wallNo = rand() % 4;
-				if (rand() % 5)
+				int wallNo;
+				if (y == 0)
 					wallNo = 0;
-
+				else {
+					wallNo = rand() % 4;
+					if (rand() % wall_chance)
+						wallNo = 0;
+				}
 				unsigned int randBlock = wall.walls[wallNo];
 				unsigned int bit_n = randBlock & 1;
 				int x_scale = x ;
 				int y_scale = y ;
-
 				board[x_scale][y_scale] = bit_n;
 				randBlock >>= 1;
 				bit_n = randBlock & 1;
@@ -141,6 +200,12 @@ public:
 			}
 		}
 
+	}
+	bool OnUserCreate() override
+	{
+		title = true;
+		wall.initWalls();
+		newLevel();
 		return true;
 	}
 	bool OnUserUpdate(float fElapsedTime) override
@@ -149,6 +214,7 @@ public:
 		olc::HWButton DOWN = GetKey(olc::Key::S);
 		olc::HWButton LEFT = GetKey(olc::Key::A);
 		olc::HWButton RIGHT = GetKey(olc::Key::D);
+		olc::HWButton FIRE = GetKey(olc::Key::SPACE);
 		olc::HWButton QUIT = GetKey(olc::Key::Q);
 		olc::HWButton PAUSE = GetKey(olc::Key::P);
 		olc::HWButton ENTER = GetKey(olc::Key::ENTER);
@@ -168,19 +234,19 @@ public:
 			}
 
 		} else {
-			if (UP.bHeld && snakeUser.ySpeed != 1.0f){
+			if (UP.bHeld && snakeUser.direction != 1){
 				snakeUser.xSpeed = 0.0f;
 				snakeUser.ySpeed = -1.0f;
 			}
-			else if (DOWN.bHeld && snakeUser.ySpeed != -1.0f){
+			else if (DOWN.bHeld && snakeUser.direction != 0){
 				snakeUser.xSpeed = 0.0f;
 				snakeUser.ySpeed = 1.0f;
 			} 
-			else if (LEFT.bHeld && snakeUser.xSpeed != 1.0f){
+			else if (LEFT.bHeld && snakeUser.direction != 3){
 				snakeUser.xSpeed = -1.0f;
 				snakeUser.ySpeed = 0.0f;
 			}
-			else if (RIGHT.bHeld && snakeUser.xSpeed != -1.0f){
+			else if (RIGHT.bHeld && snakeUser.direction != 2){
 				snakeUser.xSpeed = 1.0f;
 				snakeUser.ySpeed = 0.0f;
 			}
@@ -191,7 +257,21 @@ public:
 				exit(0);
 			}
 
-			if (last_update > snakeUser.speed) {
+			if (FIRE.bHeld && (bullet.y <= 0 || bullet.y >= ScreenHeight()) && (bullet.coolDown > 2.0f)){
+				bullet.x = snakeUser.x;
+				bullet.y = snakeUser.y;
+				if (snakeUser.xSpeed > 0)
+					bullet.direction = 3;
+				else if (snakeUser.xSpeed < 0)
+					bullet.direction = 2;
+				else if (snakeUser.ySpeed > 0)
+					bullet.direction = 1;
+				else
+					bullet.direction = 0;
+				bullet.coolDown = 0.0f;
+			}
+
+			if (last_update > gameSpeed) {
 				// std::cout << "X: " << snakeUser.x << " Y: " << snakeUser.y << " fEl: " << fElapsedTime << std::endl;
 
 				// Calculations
@@ -204,6 +284,20 @@ public:
 				snakeUser.shift();
 				snakeUser.calculatePosition();
 
+				if (bullet.x <= ScreenWidth() && bullet.x >= 0 && bullet.y <= ScreenHeight() && bullet.y >= 0){
+					if (bullet.direction == 0)
+						bullet.y += snakeUser.blockSize * -2.0f;
+					else if (bullet.direction == 1)
+						bullet.y += snakeUser.blockSize * 2.0f;
+					else if (bullet.direction == 2)
+						bullet.x += snakeUser.blockSize * -2.0f;
+					else if (bullet.direction == 3)
+						bullet.x += snakeUser.blockSize * 2.0f;
+				} else {
+					bullet.x = -10.0f;
+					bullet.y = -10.0f;
+				}
+
 				// Clear
 				FillRect(0, 0, ScreenWidth(), ScreenHeight(), olc::VERY_DARK_MAGENTA);
 
@@ -212,8 +306,13 @@ public:
 					FillRect(snakeUser.trail[i].x, snakeUser.trail[i].y, snakeUser.blockSize, snakeUser.blockSize, olc::GREEN);
 				}
 
-				for (int x = 0; x < ScreenWidth() / snakeUser.blockSize + 1; x++) {
-					for (int y = 0; y < ScreenHeight() / snakeUser.blockSize + 1; y++) {
+				if (bullet.y != -10.0f) {
+					FillRect(bullet.x, bullet.y, snakeUser.blockSize, snakeUser.blockSize, olc::RED);
+					FillRect(bullet.x + 2, bullet.y + 2, 6, 6, olc::YELLOW);
+				}
+				
+				for (int x = 0; x < ScreenWidth() / snakeUser.blockSize; x++) {
+					for (int y = 0; y < ScreenHeight() / snakeUser.blockSize; y++) {
 						if (board[x][y] == 1) {
 							FillRect(x*10,y*10, snakeUser.blockSize, snakeUser.blockSize, olc::BLUE);
 						}
@@ -235,7 +334,7 @@ public:
 								current_light_coord++;
 							}
 						} else {
-							SetPixelBlend(0.9f);
+							SetPixelBlend(0.80f);
 							FillRect(x, y, snakeUser.blockSize, snakeUser.blockSize, olc::BLACK);
 						}
 					}
@@ -244,23 +343,65 @@ public:
 				SetPixelBlend(1.0f);
 
 				// Collision
-				snakeUser.boundaryCollision(ScreenWidth(), ScreenHeight());
+				float nextX, nextY, prevX, prevY;
+				if (bullet.direction == 0){
+					nextX = prevX = bullet.x;
+					nextY = bullet.y - 10.0f;
+					prevY = bullet.y + 10.0f;
+				} else if (bullet.direction == 1){
+					nextX = prevX = bullet.x;
+					nextY = bullet.y + 10.0f;
+					prevY = bullet.y - 10.0f; 
+				} else if (bullet.direction == 2){
+					nextX = bullet.x - 10.0f;
+					nextY = prevY= bullet.y;
+					prevY = bullet.x + 10.0f;
+				} else if (bullet.direction == 3){
+					nextX = bullet.x + 10.0f;
+					nextY = prevY= bullet.y;
+					prevX = bullet.x - 10.0f;
+				}
+				if (board[int(prevX/snakeUser.blockSize)][int(prevY/snakeUser.blockSize)] == 1){
+					board[int(prevX/snakeUser.blockSize)][int(prevY/snakeUser.blockSize)] = 0;
+					FillRect(prevX, prevY, snakeUser.blockSize, snakeUser.blockSize, olc::MAGENTA);
+					bullet.x = bullet.y = -10.0f;
+				} else if (board[int(bullet.x/snakeUser.blockSize)][int(bullet.y/snakeUser.blockSize)] == 1) {
+					board[int(bullet.x/snakeUser.blockSize)][int(bullet.y/snakeUser.blockSize)] = 0;
+					FillRect(bullet.x, bullet.y, snakeUser.blockSize, snakeUser.blockSize, olc::MAGENTA);
+					bullet.x = bullet.y = -10.0f;
+				}
+
+				if (!(snakeUser.boundaryCollision(ScreenWidth(), ScreenHeight()))){
+					reset();
+					//dead
+				}
 
 				if (board[int(snakeUser.x/snakeUser.blockSize)][int(snakeUser.y/snakeUser.blockSize)] == 1){
-					std::cout << "HIT" << std::endl;
+					reset();
+					//dead
 				}
 
 				if (fruit.collision(snakeUser.x, snakeUser.y)){
+					fruit.randomLoc();
 					while (board[int(fruit.x/snakeUser.blockSize)][int(fruit.y/snakeUser.blockSize)] != 0)
 						fruit.randomLoc();
 					snakeUser.length++;
 					snakeUser.score += 5;
-				}
 
+					float level_fl = snakeUser.score / 10;
+					if (floor(level_fl) == level_fl && level != level_fl){
+						level = level_fl;
+						newLevel();
+					}
+				}
 				last_update -= gameSpeed;
 			}
 		}
-		DrawString(ScreenWidth() - 30, 5, std::to_string(snakeUser.score), olc::WHITE);
+		DrawString(5, 5, "Score " + std::to_string(snakeUser.score), olc::WHITE);
+		DrawString(115, 5, "Level " + std::to_string(level + 1), olc::WHITE);
+		DrawString(ScreenWidth()- 60, 5, "Lives " + std::to_string(snakeUser.lives), olc::WHITE);
+
+		bullet.coolDown += fElapsedTime;
 		last_update += fElapsedTime;
 		return true;
 	}
@@ -269,7 +410,7 @@ public:
 int main()
 {
 	Snake s;
-	SnakeMaze demo;
+	Anguidere demo;
 	if (demo.Construct(300, 300, 3, 3))
 		demo.Start();
 	return 0;
